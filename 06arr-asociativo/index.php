c59b7525c1a5b0c0d2f3c28625bab8447ace2c38
Sylvia Llorente <sylviall81@gmail.com>
2024-03-01 13:39:42 +0100

    ejercicio array asociativo

--- 06arr-asociativo/index.php
+++ 06arr-asociativo/index.php
@@ -1,84 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prueba php</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
-<center>
+    <h1>Array Asociativo</h1>
 
-            <h1>Sorteo</h1> </center>
+
 
     <?php
 
-    $arrayParticipantes = [
-    'Maria Lopez',
-    'Pablo Perez', 
-    'José Martinez',
-    'Victor Flores', 
-    'Marta Lozano', 
-    'Elena Mendez',
-    'Paula Sanchez',
-    'Fernanda Villa', 
-    'Cristina Gutierrez',
-    'Humberto Nuñez',
-    'Petra Alvarado',
-    'Rafael Torres',
-    'Daniel Fernandez'
+    $movie = [
+        'title' => 'The Godfather',
+        'year' => '1972',
+        'duration' => '175min.',
+        'genre' => 'drama'
     ];
 
+    print_r($movie);
+
+    echo '<br>';
+    echo '<hr>';
+
+    echo '<h3>Acceder</h3>';
+    echo 'imprimir solo valor:<br>';
+    echo $movie['title'];
+
+
+    echo '<br>';
+    echo '<hr>';
+
+    echo '<h3>Modificar</h3>';
+    echo 'modificar valor de una llave(key) genero Drama cambia a crime<br>';
+    echo $movie['genre'] = 'crime';
+    echo '<br>';
+
+    print_r($movie);
+
+    echo '<br>';
+    echo '<hr>';
+
+    echo '<h3>Añadir</h3>';
+    echo 'añadir nueva key, añado el rating de la peli<br>';
+    echo $movie['rating'] = '9.2';
+    echo '<br>';
+    print_r($movie);
+
+    echo '<br>';
+    echo '<hr>';
+
+    echo '<h3>Eliminar</h3>';
+
+    echo 'ejemplo unset: eliminar el key "rating" ';
+
+    unset($movie["rating"]); //Unset elimina ese key del array
+    echo '<br>';
+    print_r($movie);
+
+    echo '<br>';
+    echo '<hr>';
+
+
+    echo ' ejemplo array_diff:(crea un nuevo array eliminando algun valor de las key q uno le indique)';
+    echo '<br>';
+    echo '<br>';
+
+
+    echo 'array $movie:';
+    print_r($movie);
+    echo '<br>';
+    echo '<br>';
+    echo 'creo un nuevo array a partir de movie pero eliminando el valor dentro del key "genre" <br>';
+    echo 'aplico array_diff y creo $nueva_peli: <br>';
+    //array_diff Crea un nuevo array sin el/los elemento/s
+    echo '<br>';
+    $nueva_peli = array_diff($movie, ["crime"]);
+    print_r($nueva_peli);
+
+    ?>
 
 
 
 
-    $premios = 3;
-    $numParticipantes = count($arrayParticipantes);
-    echo 'ver numero de participantes:';
-    var_dump(count($arrayParticipantes));
-    
-  
-    function pickWinner( $numParticipantes, $premios) {
-        $arrayGanadores = [];
-        $indexParticipantes = $numParticipantes - 1;//hay q meterlo como variable local xq si no da error
-
-        while ( count($arrayGanadores) < $premios) {
-
-            $ganador = rand(0,$indexParticipantes);
-            // echo 'ganador:'.$ganador;
 
 
-            if (!in_array($ganador,$arrayGanadores)){
-
-                $arrayGanadores[]= $ganador;
-
-            };
-
-        }
-
-    return $arrayGanadores;
-
-    
-    }
 
 
-    $indexGanadores = pickWinner($numParticipantes,$premios);
-    // print_r($indexGanadores);
 
-    
-?>
 
-<h2>Lista de participantes:</h2>
+
+    <!-- <h2>Lista de participantes:</h2>
 
 <ol>
-    <?php 
+    <?php
 
-foreach ($arrayParticipantes as $participante){   
-    echo '<li>'.$participante.'</li>';        
-};
-   ?>
+    foreach ($arrayParticipantes as $participante) {
+        echo '<li>' . $participante . '</li>';
+    };
+    ?>
 </ol>
 
 
@@ -88,16 +113,17 @@ foreach ($arrayParticipantes as $participante){
             <h2> Ganadores:</h2>
             <ol>
         <?php
-            foreach ($indexGanadores as $numOrder){   
-                echo '<li>'.$arrayParticipantes[$numOrder].'</li>';        
-            };
+        foreach ($indexGanadores as $numOrder) {
+            echo '<li>' . $arrayParticipantes[$numOrder] . '</li>';
+        };
         ?>
             </ol>
 
-</div>
+</div> -->
 
-    
 
-   
+
+
 </body>
+
 </html>
