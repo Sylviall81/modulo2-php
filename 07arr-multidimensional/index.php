0937ac352023a55996b21a043ee46a509fe64da9
Sylvia Llorente <sylviall81@gmail.com>
2024-03-01 14:29:39 +0100

    ajustes

--- 07arr-multidimensional/index.php
+++ 07arr-multidimensional/index.php
@@ -23,72 +23,89 @@
 
     <?php
 
+
+    $agenda = [
+
     $contacto = [
         'nombre' => 'María',
         'apellido' => 'Olmo',
+        'teléfono' => '616260746',
+        'email' => 'fake@mail.com'
+    ],
+    $contacto = [
+        'nombre' => 'Elena',
+        'apellido' => 'Gutierrez',
         'teléfono' => '618240976',
         'email' => 'fake@mail.com'
-    ];
+    ],
+    $contacto = [
+        'nombre' => 'Pablo',
+        'apellido' => 'Rodriguez',
+        'teléfono' => '618240976',
+        'email' => 'fake@mail.com'
+    ],
 
-    print_r($movie);
+    ]
 
-    echo '<br>';
-    echo '<hr>';
+    // print_r($movie);
 
-    echo '<h3>Acceder</h3>';
-    echo 'imprimir solo valor:<br>';
-    echo $movie['title'];
+    // echo '<br>';
+    // echo '<hr>';
 
-
-    echo '<br>';
-    echo '<hr>';
-
-    echo '<h3>Modificar</h3>';
-    echo 'modificar valor de una llave(key) genero Drama cambia a crime<br>';
-    echo $movie['genre'] = 'crime';
-    echo '<br>';
-
-    print_r($movie);
-
-    echo '<br>';
-    echo '<hr>';
-
-    echo '<h3>Añadir</h3>';
-    echo 'añadir nueva key, añado el rating de la peli<br>';
-    echo $movie['rating'] = '9.2';
-    echo '<br>';
-    print_r($movie);
-
-    echo '<br>';
-    echo '<hr>';
-
-    echo '<h3>Eliminar</h3>';
-
-    echo 'ejemplo unset: eliminar el key "rating" ';
-
-    unset($movie["rating"]); //Unset elimina ese key del array
-    echo '<br>';
-    print_r($movie);
-
-    echo '<br>';
-    echo '<hr>';
+    // echo '<h3>Acceder</h3>';
+    // echo 'imprimir solo valor:<br>';
+    // echo $movie['title'];
 
 
-    echo ' ejemplo array_diff:(crea un nuevo array eliminando algun valor de las key q uno le indique)';
-    echo '<br>';
-    echo '<br>';
+    // echo '<br>';
+    // echo '<hr>';
+
+    // echo '<h3>Modificar</h3>';
+    // echo 'modificar valor de una llave(key) genero Drama cambia a crime<br>';
+    // echo $movie['genre'] = 'crime';
+    // echo '<br>';
+
+    // print_r($movie);
+
+    // echo '<br>';
+    // echo '<hr>';
+
+    // echo '<h3>Añadir</h3>';
+    // echo 'añadir nueva key, añado el rating de la peli<br>';
+    // echo $movie['rating'] = '9.2';
+    // echo '<br>';
+    // print_r($movie);
+
+    // echo '<br>';
+    // echo '<hr>';
+
+    // echo '<h3>Eliminar</h3>';
+
+    // echo 'ejemplo unset: eliminar el key "rating" ';
+
+    // unset($movie["rating"]); //Unset elimina ese key del array
+    // echo '<br>';
+    // print_r($movie);
+
+    // echo '<br>';
+    // echo '<hr>';
 
 
-    echo 'array $movie:';
-    print_r($movie);
-    echo '<br>';
-    echo '<br>';
-    echo 'creo un nuevo array a partir de movie pero eliminando el valor dentro del key "genre" <br>';
-    echo 'aplico array_diff y creo $nueva_peli: <br>';
-    //array_diff Crea un nuevo array sin el/los elemento/s
-    echo '<br>';
-    $nueva_peli = array_diff($movie, ["crime"]);
-    print_r($nueva_peli);
+    // echo ' ejemplo array_diff:(crea un nuevo array eliminando algun valor de las key q uno le indique)';
+    // echo '<br>';
+    // echo '<br>';
+
+
+    // echo 'array $movie:';
+    // print_r($movie);
+    // echo '<br>';
+    // echo '<br>';
+    // echo 'creo un nuevo array a partir de movie pero eliminando el valor dentro del key "genre" <br>';
+    // echo 'aplico array_diff y creo $nueva_peli: <br>';
+    // //array_diff Crea un nuevo array sin el/los elemento/s
+    // echo '<br>';
+    // $nueva_peli = array_diff($movie, ["crime"]);
+    // print_r($nueva_peli);
 
     ?>
 
