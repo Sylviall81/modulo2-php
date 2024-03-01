68555008a67e5763e2faf0a3d889e0725239bbf2
Sylvia Llorente <sylviall81@gmail.com>
2024-03-01 11:19:33 +0100

    ejercicio ganadores

--- index.php
+++ index.php
@@ -9,14 +9,82 @@
 
 <center>
 
-            <h1>PHP Info</h1> </center>
+            <h1>Sorteo</h1> </center>
 
     <?php
-   phpinfo();
 
-//    echo '<h1>HOLA MUNDO</h1>';
+    $arrayParticipantes = [
+    'Maria Lopez',
+    'Pablo Perez', 
+    'José Martinez',
+    'Victor Flores', 
+    'Marta Lozano', 
+    'Elena Mendez',
+    'Paula Sanchez',
+    'Fernanda Villa', 
+    'Cristina Gutierrez',
+    'Humberto Nuñez',
+    'Petra Alvarado',
+    'Rafael Torres'
+    ];
 
-//    print '<h2>texto con print</h2>';
+
+
+
+
+    $premios = 3;
+    $numParticipantes = count($arrayParticipantes);
+    //var_dump(count($arrayParticipantes));
+    $indexParticipantes = $numParticipantes - 1; //posicion de los participantes dentro del array
+    //var_dump(($indexParticipantes));
+
+
+    function pickWinner( $numParticipantes, $premios) {
+
+        $arrayGanadores = [];
+
+
+        while ( count($arrayGanadores) < $premios) {
+
+            $ganador = rand(0,$numParticipantes);
+            echo 'ganador:'.$ganador;
+
+
+            if (!in_array($ganador,$arrayGanadores)){
+
+                $arrayGanadores[]= $ganador;
+
+            };
+
+        }
+
+    return $arrayGanadores;
+
+    
+    }
+
+
+    $indexGanadores = pickWinner($numParticipantes,$premios);
+    print_r($indexGanadores);
+
+
+    echo("prueba array ganadores posiciones");
+    
+
+    echo '<h2> Felicidades a los ganadores:</h2> <br> <ol>';
+
+    foreach ($indexGanadores as $numOrder){    
+        echo ' <li>'.$arrayParticipantes[$numOrder].'</li>';        
+    };
+
+    echo '</ol>';
+
+    
+
+
+
+  
+   
 
     ?>
 
