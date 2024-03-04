6eaf920b36187624a89780750fcf4ffb9474825a
Sylvia Llorente <sylviall81@gmail.com>
2024-03-04 12:41:23 +0100

    ejercicios extra variables

--- 01variables/index.php
+++ 01variables/index.php
@@ -48,15 +48,64 @@
         $ciudad_actual= 'Barcelona';
 
         echo $ciudad_actual.'</br>';
-        exit;
+       
        
         ?>
 
-<h3>Ejercicio 2</h3>
+<h3>Extra variables Odin Project</h3>
 
-<p>Desarrollar un programa en PHP que calcule el área de un triángulo rectángulo, y la hipotenusa.</p>
+<p>Try the following exercises:
+
+<ol>
+
+<li>
+Add 2 numbers together!</li>
+<li>
+Add a sequence of 6 different numbers together.</li>
+
+<li>Print the value of the following expression: (4 + 6 + 9) / 77</li>
+<li>Let’s use variables!
+Type and print variable a = 10</li>
+
+
+<li>try 9 * a;</li>
+
+<li> b = 7 * a </li>
+
+<li>print b</li>
+
+<li>declare variable $max with value 57</li>
+<li>another variable $actual with value max-13</li>
+<li>Set another variable percentage to actual / MAX</li>
+<li>print percentage</li>
+
+</ol>
+
+</p>
 
 <?php 
+
+echo 'suma dos numeros';
+echo '<br>';
+$suma = 6+3;
+echo $suma;
+echo '<br>';
+
+echo 'suma 6 numeros';
+echo '<br>';
+$sumaSix = 3+4+5+6+7+8;
+echo $sumaSix;
+echo '<br>';
+
+echo 'print value of (4 + 6 + 9)/77 <br>';
+$value = 4+6+9 / 77;
+echo  'resultado: '.$value;
+echo '<br>';
+
+echo 'var a = 10 <br>';
+$a = 10;
+echo  'a: '.$a;
+echo '<br>';
 
 
 
