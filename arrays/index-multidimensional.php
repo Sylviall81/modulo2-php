c227942df8d64669d216f2269480f93178b77db4
Sylvia Llorente <sylviall81@gmail.com>
2024-03-07 14:35:33 +0100

    arrays multidimensionales

---
+++ arrays/index-multidimensional.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Multidimensional</title>
+</head>
+<body>
+    <h1>Array Multidimensional</h1>
+
+
+    <?php
+
+$listaCoches = array(
+    "modelo"=> "Solterra",
+    "marca" => "Subaru",
+    "colores"=> array("verde","azul", "rojo"),
+    "año" => array ("2008", "2000")
+
+);
+
+echo "Array Original:<br>";
+print_r($listaCoches);
+echo "<br>";
+
+foreach($listaCoches as $key){
+    // print_r ( "key: ".$key.'<br>');
+    foreach($key as $categoria => $val){
+        echo "<br>";
+        echo $categoria." ".$val."<br>";
+    }
+}
+
+
+    
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
