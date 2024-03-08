1dbaec8cf34c539a911e1e1cdb7b7262f4c77f37
Sylvia Llorente <sylviall81@gmail.com>
2024-03-08 12:00:01 +0100

    laberinto

--- arrays/index-ejercicio.php
+++ arrays/index-ejercicio.php
@@ -1,78 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio array</title>
 </head>
+
 <body>
 
-<?php
+    <?php
 
-$estudiantes = array(
-    array(
-        "nombre" => "Juan",
-        "edad" => 20,
-        "notas" => array(9, 8, 6)
-    ),
-    array(
-        "nombre" => "María",
-        "edad" => 22,
-        "notas" => array(7, 9, 6)
-    ),
-    array(
-        "nombre" => "Carlos",
-        "edad" => 21,
-        "notas" => array(8, 9, 7)
-    ),
-    array(
-        "nombre" => "Laura",
-        "edad" => 23,
-        "notas" => array(6, 8, 9)
-    )
-);
+    $estudiantes = array(
+        array(
+            "nombre" => "Juan",
+            "edad" => 20,
+            "notas" => array(9, 8, 6)
+        ),
+        array(
+            "nombre" => "María",
+            "edad" => 22,
+            "notas" => array(7, 9, 6)
+        ),
+        array(
+            "nombre" => "Carlos",
+            "edad" => 21,
+            "notas" => array(8, 9, 7)
+        ),
+        array(
+            "nombre" => "Laura",
+            "edad" => 23,
+            "notas" => array(6, 8, 9)
+        )
+    );
 
-$edadMaria = $estudiantes[1]['edad'];
-echo 'La edad de María es '.$edadMaria.'<br>';
+    $edadMaria = $estudiantes[1]['edad'];
+    echo 'La edad de María es ' . $edadMaria . '<br>';
 
-$notaCarlos = $estudiantes[2]['notas'][1];
-echo 'La 2a nota de Carlos es '.$notaCarlos.'<br>';
+    $notaCarlos = $estudiantes[2]['notas'][1];
+    echo 'La 2a nota de Carlos es ' . $notaCarlos . '<br>';
 
 
-echo "El nombre de los estudiantes que participan son:<br>";
- 
-foreach ($estudiantes as $estudiante){
+    echo "El nombre de los estudiantes que participan son:<br>";
 
-    echo $estudiante['nombre'].'<br>';
-   
-    
-}
-echo '<br>';
+    foreach ($estudiantes as $estudiante) {
 
-$nombresEstudiantes = array_column($estudiantes, "nombre");
-echo "nombre de los estudiantes: ".implode(',', $nombresEstudiantes);
-echo '<br>';
+        echo $estudiante['nombre'] . '<br>';
+    }
+    echo '<br>';
 
-$edadEstudiantes = array_column($estudiantes, "edad");
-echo "edad de los estudiantes: ".implode(',', $edadEstudiantes);
-echo '<br>';
+    $nombresEstudiantes = array_column($estudiantes, "nombre");
+    echo "nombre de los estudiantes: " . implode(',', $nombresEstudiantes);
+    echo '<br>';
 
+    $edadEstudiantes = array_column($estudiantes, "edad");
+    echo "edad de los estudiantes: " . implode(',', $edadEstudiantes);
+    echo '<br>';
+    echo '<br>';
 
 
 
 
-foreach ($estudiantes as $datos ){
 
-        echo "estudiante :".$datos['nombre'].'<br>';
-    foreach ($datos['notas'] as $notas){
+    foreach ($estudiantes as $estudiante) {
 
-        echo " notas: ".$notas.',';
-        echo '<br>';
+        echo "estudiante :" . $estudiante['nombre'] . '<br>';
+
+        foreach ($estudiante as $key => $val) {
+            echo "notas :" . $key . '<br>';
+
+
+            foreach ($estudiante as $nota => $val) {
+
+
+                echo " notas: " . $nota . ',';
+                echo '<br>';
+                echo "suma notas: " . array_sum($estudiante['notas']) . '<br>';
+                echo '<br>';
+                echo "largo de array: " . count($estudiante['notas']) . '<br>';
+                echo '<br>';
+                echo "media: " . array_sum($estudiante['notas']) / count($estudiante['notas']);
+            }
+        }
     }
 
-}
-
-        
 
 
 
@@ -82,10 +93,17 @@ foreach ($estudiantes as $datos ){
 
 
 
-?>
 
 
 
-    
+
+
+
+    ?>
+
+
+
+
 </body>
+
 </html>
---
+++ arrays/laberinto.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<p> cuenta el número de puntos con un loop: </p>
+
+<br>
+<div>
+    "#", "#", "#", "#", "#"<br>
+    "#", ".", ".", ".", "#"<br>
+    "#", ".", "#", ".", "#"<br>
+    "#", ".", ".", ".", "#"<br>
+    "#", "#", "#", "#", "#"<br>
+    <br>
+    </div>
+
+<?php
+$laberinto = array(
+    array("#", "#", "#", "#", "#"),
+    array("#", ".", ".", ".", "#"),
+    array("#", ".", "#", ".", "#"),
+    array("#", ".", ".", ".", "#"),
+    array("#", "#", "#", "#", "#")
+);
+
+$dotCount = 0;
+$hashtagCount = 0;
+
+foreach ($laberinto as $line){
+    foreach ($line as $item ){
+
+        if($item === '.'){
+            $dotCount++;
+            //echo "dotcount: ".$dotCount."<br>";
+        } else {
+
+            $hashtagCount ++;
+            //echo $hashtagCount;
+            //echo "hashtagcount: ".$hashtagCount."<br>";
+
+        }
+    }
+}
+
+echo "<br>";
+echo '<strong> RESULTADO</strong></strong><br>';
+echo "el numero de puntos es: ".$dotCount."<br>";
+echo "el numero de almohadillas es: ".$hashtagCount."<br>";
+
+?>
+    
+</body>
+</html>
