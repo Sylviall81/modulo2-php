6d433653505bbc27ce1fcf2b8e8dac5b9f9f5bb1
Sylvia Llorente <sylviall81@gmail.com>
2024-03-18 10:17:54 +0100

    solucion grid 3

--- practicaGrid3/grid3.php
+++ practicaGrid3/grid3.php
@@ -11,40 +11,33 @@
 <?php
 
 $arrayContent = array (
-	array ( "¿Cómo somos?","Honestos, ágiles y eficientes.","./imagenes/que_se_nos_da_bien.png"),
-	array ( "¿Que se nos da bien?","Dotar a las compañías de las soluciones que lideran el mercado, asegurando el éxito de la implantación y su calidad.","./imagenes/llegar.png"),
-	array ( "¿Cómo hemos llegado hasta aquí?","Con pasión y esfuerzo en nuestro trabajo, y con la colaboración de nuestros clientes desde hace más de 25 años.","./imagenes/que_se_nos_da_bien.png"),
-	array ( "¿Cómo trabajamos?","Con nuestra propia metodología que implanta que nos asegura el éxito y la calidad de los proyectos, implantando las soluciones de manera más efectiva.","./imagenes/quienes_somos.png"),
+	array ( "¿Cómo somos?","Honestos, ágiles y eficientes.","./imagenes/que_se_nos_da_bien.png",""),
+	array ( "¿Que se nos da bien?","Dotar a las compañías de las soluciones que lideran el mercado, asegurando el éxito de la implantación y su calidad.","./imagenes/llegar.png","ordenInverso"),
+	array ( "¿Cómo hemos llegado hasta aquí?","Con pasión y esfuerzo en nuestro trabajo, y con la colaboración de nuestros clientes desde hace más de 25 años.","./imagenes/que_se_nos_da_bien.png",""),
+	array ( "¿Cómo trabajamos?","Con nuestra propia metodología que implanta que nos asegura el éxito y la calidad de los proyectos, implantando las soluciones de manera más efectiva.","./imagenes/quienes_somos.png","ordenInverso"),
 
 );
 
 var_dump($arrayContent);
 
+$clase=""; //Intentar o mirar la solucion con if
 
-?>
-	
 
-	<?php 
-	foreach ($arrayContent as $row) {?>
-	
+	foreach ($arrayContent as $row) {
+
+	?>
 	<div class="caja">
-		<img src="<?php echo $arrayContent[$row][2] //imagen?>"> 
+		<img src="<?php echo $row[2] //imagen?>" class="<?php echo $row[3]?>"> 
 		<div class="texto">
-			<h2><?php echo $arrayContent[$row][0] //titulo?></h2>
-			<p><?php echo $arrayContent[$row][1] //descripcion?></p>
+			<h2><?php echo $row[0] //titulo?></h2>
+			<p><?php echo $row[1] //descripcion?></p>
 		</div>
 	</div>
 	<?php  }
-
-
-		
-	
 	
 	?>
 
 	 
 	
-	
-
 </body>
 </html>
