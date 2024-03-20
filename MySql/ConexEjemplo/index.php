9c7140fb76ff9af55442d910275d002d3b09f71c
Sylvia Llorente <sylviall81@gmail.com>
2024-03-20 22:24:07 +0100

    display errors

--- MySql/ConexEjemplo/index.php
+++ MySql/ConexEjemplo/index.php
@@ -1,6 +1,10 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "usuario_erp", "45960967Kk*")
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+$conexion = mysqli_connect("localhost", "user_erp", "45960967Kk*")
 or die ("No se ha podido conectar a la base de datos");
 
 echo "Conectado a la base de datos";
