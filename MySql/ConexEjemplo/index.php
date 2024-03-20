3c96bc418b617c8c6addf7d5e85887fb7b6de36c
Sylvia Llorente <sylviall81@gmail.com>
2024-03-20 22:26:42 +0100

    variables

--- MySql/ConexEjemplo/index.php
+++ MySql/ConexEjemplo/index.php
@@ -4,7 +4,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$conexion = mysqli_connect("localhost", "user_erp", "45960967Kk*")
+$server = "localhost";
+$user = "user_erp";
+$psswd = "45960967Kk*";
+
+$conexion = mysqli_connect($server,$user,$psswd)
 or die ("No se ha podido conectar a la base de datos");
 
 echo "Conectado a la base de datos";
